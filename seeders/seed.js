6c001b7cbbc1a1b1704c4eb529d4cc23b87b506f
Brittany Fortner <brittany.e.fortner@gmail.com>
2020-03-27 09:48:47 -0700

    Update seed.js file with db connection

--- seeders/seed.js
+++ seeders/seed.js
@@ -2,9 +2,8 @@ let mongoose = require("mongoose");
 let db = require("../models");
 const moment = require("moment");
 
-mongoose.connect("mongodb://localhost/workout", {
-    useNewUrlParser: true,
-    useFindAndModify: false
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true
 });
 
 // seed to get database filled for immediate data visualisation
